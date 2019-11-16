13517a2fe6dc1e244031018cfdd9a4b3512b7fe2
Emily Kapor-Mater <emily@emilyaviva.com>
2019-11-15 23:18:39 -0800

    Add edit mode to data rows

--- src/components/unicorn-row.js
+++ src/components/unicorn-row.js
@@ -6,34 +6,80 @@ class UnicornRow extends React.Component {
     super(props)
 
     this.state = {
+      editMode: false,
       location: this.props.location
     }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleEdit = this.handleEdit.bind(this)
+    this.handleCancel = this.handleCancel.bind(this)
+    this.handleSave = this.handleSave.bind(this)
   }
 
   handleChange (e) {
+    const target = e.target
+    const value = target.value
+    const name = target.name
     this.setState({
-      location: e.target.value
+      [name]: value
+    })
+  }
+
+  handleEdit () {
+    this.setState({
+      editMode: true
+    })
+  }
+
+  handleCancel () {
+    this.setState({
+      editMode: false
+    })
+  }
+
+  handleSave () {
+    this.setState({
+      editMode: false
     })
   }
 
   render () {
     const { name, color, food } = this.props
-    const { location } = this.state
-    return (
-      <tr>
-        <td>{name}</td>
-        <td>{color}</td>
-        <td>{food}</td>
-        <td>
-          <select value={location} onChange={this.handleChange.bind(this)}>
-            <option value='Barn'>Barn</option>
-            <option value='Trails'>Trails</option>
-            <option value='Pasture'>Pasture</option>
-            <option value='Swimming Pool'>Swimming Pool</option>
-          </select>
-        </td>
-      </tr>
-    )
+    const { location, editMode } = this.state
+
+    if (!editMode) {
+      return (
+        <tr>
+          <td>{name}</td>
+          <td>{color}</td>
+          <td>{food}</td>
+          <td>{location}</td>
+          <td>
+            <button onClick={this.handleEdit}>Edit</button>
+          </td>
+        </tr>
+      )
+    } else {
+      return (
+        <tr>
+          <td><input type='text' value={name} /></td>
+          <td><input type='text' value={color} /></td>
+          <td><input type='text' value={food} /></td>
+          <td>
+            <select value={location} onChange={this.handleChange}>
+              <option value='Barn'>Barn</option>
+              <option value='Trails'>Trails</option>
+              <option value='Pasture'>Pasture</option>
+              <option value='Swimming Pool'>Swimming Pool</option>
+            </select>
+          </td>
+          <td>
+            <button onClick={this.handleSave}>Save</button>
+            <button onClick={this.handleCancel}>Cancel</button>
+          </td>
+        </tr>
+      )
+    }
   }
 }
 
